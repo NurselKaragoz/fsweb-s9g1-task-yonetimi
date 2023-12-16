46c42a2172f7b22454835507235f33057fa8d78b
Nursel Karagoz <nurselkaragoz@outlook.com>
2023-12-16 23:36:44 +0300

    taskhookform eklendi

--- src/Task.js
+++ src/Task.js
@@ -1,16 +1,24 @@
 import React from "react";
+import { toast } from "react-toastify";
 
 const Task = ({ taskObj, onComplete }) => {
+  const handleComplete = () => {
+    onComplete(taskObj.id);
+    toast.success("Task completed successfully!");
+  };
+
   return (
     <div className="task">
       <h3>{taskObj.title}</h3>
       <p>{taskObj.description}</p>
       <div>
         {taskObj.people.map((p) => (
-          <span className="pill" key={p}>{p}</span>
+          <span className="pill" key={p}>
+            {p}
+          </span>
         ))}
       </div>
-      {onComplete && <button onClick={() => onComplete(taskObj.id)}>Tamamlandı</button>}
+      {onComplete && <button onClick={handleComplete}>Tamamlandı</button>}
     </div>
   );
 };
--- src/TaskHookForm.js
+++ src/TaskHookForm.js
@@ -1,7 +1,103 @@
-import React from 'react'
+import React from "react";
+import { useForm } from "react-hook-form";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-export default function TaskHookForm() {
+export default function TaskHookForm({ kisiler, submitFn }) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    submitFn({ ...data, status: "yapılacak" });
+  };
+
+  const Success = () => {
+    if (register) {
+      toast("Yeni Göreviniz Eklendi !");
+    }
+  };
+
   return (
-    <div>Formunuzu react-hook-form kullanarak burada oluşturun. TaskForm dosyasındaki HTML yapısını vs app.css içerisindeki classları kullanabilirsiniz.</div>
-  )
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="title" className="input-label">
+          Başlık
+        </label>
+        <input
+          className="input-text"
+          type="text"
+          id="title"
+          placeholder="Title"
+          {...register("title", {
+            required: "Başlık alanı boş bırakılamaz !",
+            minLength: {
+              value: 3,
+              message: "Başlık en az 3 karakter içermelidir",
+            },
+          })}
+        />
+        {errors.title && <p className="input-error">{errors.title.message}</p>}
+      </div>
+      <div className="form-line">
+        <label htmlFor="description" className="input-label">
+          Açıklama :
+        </label>
+        <input
+          className="input-textarea"
+          type="text"
+          id="description"
+          placeholder="Açıklama"
+          {...register("description", {
+            required: "Mutlaka Açıklama Yazmalısınız!",
+            minLength: {
+              value: 10,
+              message: "Açıklama en az 10 karakter içermelidir",
+            },
+          })}
+        />
+        {errors.description && (
+          <p className="input-error">{errors.description.message}</p>
+        )}
+      </div>
+      <div className="form-line">
+        <label className="input-label">İnsanlar </label>
+        {kisiler.map((kisi, i) => (
+          <div key={i}>
+            <input
+              className="input-checkbox"
+              value={kisi}
+              type="checkbox"
+              {...register("people", {
+                required: "En az bir kişi seçmelisiniz !",
+                validate: {
+                  maxThree: (value) =>
+                    value.length <= 3 || "En fazla 3 kişi seçebilirsiniz!",
+                },
+              })}
+            />
+            {kisi}
+          </div>
+        ))}
+        {errors.people && (
+          <p className="input-error">{errors.people.message}</p>
+        )}
+      </div>
+      <div>
+        <input
+          type="submit"
+          className="submit-button"
+          value="Görevi Ekle"
+          onClick={Success}
+        />
+      </div>
+    </form>
+  );
 }
+
+// export default function TaskHookForm() {
+//   return (
+//     <div>Formunuzu react-hook-form kullanarak burada oluşturun. TaskForm dosyasındaki HTML yapısını vs app.css içerisindeki classları kullanabilirsiniz.</div>
+//   )
